89dc27140905ecc03ec65b17f3eefc0fb2f5afcd
daemon1024 <barun1024@gmail.com>
2020-12-04 22:47:20 +0530

    optimise day 1 code

--- day1/part1.js
+++ day1/part1.js
@@ -2,5 +2,5 @@ const data = await Deno.readTextFile("./input.txt");
 let numbers = data.split("\n");
 numbers = numbers.map(Number);
 numbers.some((v1) =>
-  numbers.some((v2) => v1 + v2 == 2020 && (console.log(v1 * v2) || true))
+  numbers.includes(2020 - v1) && (console.log(v1 * (2020 - v1)) || true)
 );
--- day1/part2.js
+++ day1/part2.js
@@ -3,8 +3,7 @@ let numbers = data.split("\n");
 numbers = numbers.map(Number);
 numbers.some((v1) =>
   numbers.some((v2) =>
-    numbers.some((v3) =>
-      v1 + v2 + v3 == 2020 && (console.log(v1 * v2 * v3) || true)
-    )
+    numbers.includes(2020 - v1 - v2) &&
+    (console.log(v1 * v2 * (2020 - v1)) || true)
   )
 );
